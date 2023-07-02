053b39573a56892b2572a40c33ecdc208b88f9eb
ps-kate <ekaterina@passion.io>
2023-07-02 12:17:20 +0100

    feat: format dates in DayList

--- src/components/DayList.tsx
+++ src/components/DayList.tsx
@@ -1,12 +1,20 @@
-import { useStore } from "../App";
+import { useStore } from "../RootStore";
 import DayItem from "./DayItem";
+import moment from "moment";
+import { useMemo } from "react";
 
 const DayList = () => {
   const rootStore = useStore();
 
+  // it's better to have this memoized, so that it doesn't recompute on every render. Moment is a bit heavy.
+  const days = useMemo(
+    () => rootStore.days.map((d) => moment(d, moment.HTML5_FMT.DATE).format("MMM Do YYYY")),
+    [rootStore.days],
+  );
+
   return (
     <div className="dayList">
-      {rootStore.days.map((d) => (
+      {days.map((d) => (
         <DayItem day={d} key={d} />
       ))}
     </div>

5a1d4434098315bcce2187146ddb9156364587dc
ps-kate <ekaterina@passion.io>
2023-07-02 14:02:56 +0100

    feat: disable times that are in the past or within 2 hours of current time

--- src/components/TimeItem.tsx
+++ src/components/TimeItem.tsx
@@ -3,6 +3,10 @@ import moment from "moment";
 import { observer } from "mobx-react-lite";
 import { useStore } from "../RootStore";
 
+// TODO: this should be created once and updated every minute
+// derived should depend on this
+const currentTime = moment();
+
 interface Props {
   time: number;
 }
@@ -10,16 +14,24 @@ interface Props {
 const TimeItem = ({ time }: Props) => {
   const { setSelectedTime, selectedTime } = useStore();
 
-  const formattedTime = useMemo(() => {
-    const hours = Math.floor(time);
-    const minutes = Math.round((time - hours) * 60);
-    return moment({ hours, minutes }).format("H:mm");
+  const timeMoment = useMemo(() => {
+    const hour = Math.floor(time);
+    const minute = Math.round((time - hour) * 60);
+    return moment().hour(hour).minute(minute);
   }, [time]);
+
+  const formattedTime = useMemo(() => {
+    return timeMoment.format("H:mm");
+  }, [timeMoment]);
 
   const classNames = ["dayItem", selectedTime === time ? "selected" : ""].join(" ");
 
+  const isDisabled = useMemo(() => {
+    return timeMoment.diff(currentTime, "hours") < 2;
+  }, [timeMoment]);
+
   return (
-    <button className={classNames} onClick={() => setSelectedTime(time)}>
+    <button className={classNames} onClick={() => setSelectedTime(time)} disabled={isDisabled}>
       {formattedTime}
     </button>
   );
